a8db8e6343087dbc2281beeccc42ff5f2f9fca41
Sebastian Cruz <165743365+1SSeba@users.noreply.github.com>
2024-08-20 22:50:40 -0400

    1

--- src/pages/api/cords.js
+++ src/pages/api/cords.js
@@ -1,13 +1,17 @@
-// src/pages/api/get_gps_data.js
+// src/pages/api/cords.js
+
 import fs from 'fs';
 import path from 'path';
 
-export async function handler(req, res) {
+export default async function handler(req, res) {
+  console.log('API endpoint hit'); // Verifica si el endpoint está siendo alcanzado
   const filePath = path.resolve('./data/gps_data.json');
   if (fs.existsSync(filePath)) {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    console.log('Data:', data); // Verifica los datos leídos del archivo
     res.status(200).json(data);
   } else {
+    console.log('File not found');
     res.status(200).json({ lat: '0', lon: '0' });
   }
 }
