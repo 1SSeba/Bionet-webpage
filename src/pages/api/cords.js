cb0de63b39cb9c36618e92209e0d33be8e5824f7
Sebastian Cruz <165743365+1SSeba@users.noreply.github.com>
2024-08-20 23:11:38 -0400

    1

--- src/pages/api/cords.js
+++ src/pages/api/cords.js
@@ -4,14 +4,15 @@ import fs from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
-  console.log('API endpoint hit'); // Verifica si el endpoint está siendo alcanzado
-  const filePath = path.resolve('./data/gps_data.json');
-  if (fs.existsSync(filePath)) {
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-    console.log('Data:', data); // Verifica los datos leídos del archivo
-    res.status(200).json(data);
-  } else {
-    console.log('File not found');
-    res.status(200).json({ lat: '0', lon: '0' });
+  try {
+    const filePath = path.resolve('./data/gps_data.json');
+    if (fs.existsSync(filePath)) {
+      const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ error: 'File not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 }
