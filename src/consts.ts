f29b8b6e0105dc3b2706906ab0a9e42ee54be722
Sebastian Cruz <165743365+1SSeba@users.noreply.github.com>
2024-08-21 21:35:32 -0400

    1

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Bionet';
-export const SITE_DESCRIPTION = 'Basti gei^2';
+export const SITE_DESCRIPTION = 'https://i.imgur.com/okyI8iq.png';
